a94b0cb0712c6d681ba2595e762bf7efa6afcff9
Rokas <rookas.rudzenskas@gmail.com>
2021-06-28 09:28:02 +0300

    Rokas

--- src/components/SearchBarAndTheJobs.js
+++ src/components/SearchBarAndTheJobs.js
@@ -30,6 +30,23 @@ const SearchBarAndTheJobs = () => {
                             <img src="https://www.gstatic.com/devrel-devsite/prod/v29345390480661f31f1340a20aede1ba9b8f625badf7470697c2e7672c91fc58/firebase/images/touchicon-180.png" alt=""/>
                             <MoreHorizIcon />
                         </div>
+
+                        <div className="rolename">
+                            <span>Lead Product Design</span>
+                        </div>
+
+                        <div className="description">
+                            <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur cumque deserunt enim eum labore nostrum quaerat repellat sequi tempora?</span>
+                        </div>
+
+                        <div className="buttons">
+                            <div className="button apply-now">
+                                Apply Now
+                            </div>
+                            <div className="button">
+                                Message
+                            </div>
+                        </div>
                     </div>
 
                     <div className="job-title">
