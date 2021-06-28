1dcfc7360b24fefec2f6dcc54be0869071a75ade
Rokas <rookas.rudzenskas@gmail.com>
2021-06-28 07:43:19 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-
+      <Header />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ import "./styles/Header.css";
 const Header = () => {
     return (
         <div className="header">
-            
+            <div className="header__container">
+                <div className="header__logo">
+                    <img src="https://pbs.twimg.com/profile_images/1350895249678348292/RS1Aa0iK.jpg" alt=""/>
+                </div>
+
+                <span>Smart Jobs</span>
+            </div>
         </div>
     );
 };
