b8daa387dabdaf62e9c7a6a3f65a4a529392adc9
Rokas <rookas.rudzenskas@gmail.com>
2021-06-28 08:05:30 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header";
+import SearchBarAndTheJobs from "./components/SearchBarAndTheJobs";
 
 function App() {
   return (
     <div className="app">
       <Header />
+        <SearchBarAndTheJobs />
     </div>
   );
 }
--- src/components/SearchBarAndTheJobs.js
+++ src/components/SearchBarAndTheJobs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
 
 const SearchBarAndTheJobs = () => {
     return (
@@ -6,11 +7,12 @@ const SearchBarAndTheJobs = () => {
             <div className="search">
                 <div className="search__container">
                     <div className="search__inputContainer">
-
+                        <SearchIcon />
+                        <input placeholder="Filter by title 🚀" name="filter-jobs" type="text" className="filter-jobs"/>
                     </div>
 
                     <div className="search__buttonContainer">
-
+                        <span>Find a Job</span>
                     </div>
                 </div>
             </div>
