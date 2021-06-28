2a4d70aa981558a5f9e90789e1844348fb388369
Rokas <rookas.rudzenskas@gmail.com>
2021-06-28 08:01:41 +0300

    Rokas

---
+++ src/components/SearchBarAndTheJobs.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchBarAndTheJobs = () => {
+    return (
+        <div className="searchBarAndTheJobs">
+            <div className="search">
+                <div className="search__container">
+                    <div className="search__inputContainer">
+
+                    </div>
+
+                    <div className="search__buttonContainer">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchBarAndTheJobs;
